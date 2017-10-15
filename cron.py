d4943eb40f5341435e49e83c1dec0a1642f01305
FlameFractal <vishg8@gmail.com>
2017-10-15 09:56:37 +0530

    added telegram commands, rewrote routes and important functions

--- cron.py
+++ cron.py
@@ -1,5 +1,6 @@
 import urllib.request
 
+domainUrl = 'https://fast-falls-57984.herokuapp.com'
 botToken = '439142723:AAGxI51LsPuv0dgzta0lGgH1aJLZfIuDTvE';
 
-urllib.request.urlopen('https://fast-falls-57984.herokuapp.com/sendMenu'+botToken)
+urllib.request.urlopen(domainUrl+'/sendMenuAllUsers'+botToken)
